41eaebf30d6fcf72f4e0bed6a40e517cf8e549b0
Daniyal Ahmed <da9033431@gmail.com>
2025-06-17 12:00:00 +0100

    refactoring

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,7 @@
 import { createBrowserRouter } from "react-router";
-import HomePage from "@/pages/HomePage";
 import { LoginPage } from "@/features/auth";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
   {
     path: "/login",
     element: <LoginPage />,
